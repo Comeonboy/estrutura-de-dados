f96b387e03bd8e90ae6e43492fe93120723d1c85
Rodolfo Alves <rodolfo.silva28@fatec.sp.gov.br>
2016-02-29 09:32:45 -0300

    Added files via upload

---
+++ maioremenor.py
@@ -0,0 +1,28 @@
+import time
+
+def maxmin(seq):
+    if len(seq)==0:
+        return maior, menor
+    if len(seq)==1:
+        return seq[0], seq[0]
+    maior, menor = maioremenor((len(seq)-1), seq)
+    return maior, menor;
+
+def maioremenor(tamanho, seq):
+    if tamanho != 0:
+        maior, menor = maioremenor(tamanho-1, seq)
+    else:
+        maior = seq[0]
+        menor = seq[0]
+        
+    if seq[tamanho] > maior:
+        maior = seq[tamanho]
+    if seq[tamanho] < menor:
+        menor = seq[tamanho]
+
+    return maior, menor;
+
+seq = [5, 0, 134, 45, 6, 21, 23]
+maior, menor = maxmin(seq)
+print(maior, menor)
+    
